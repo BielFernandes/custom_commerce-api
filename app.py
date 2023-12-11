e1d917b7d4aa83dc674bc9129bd6dc041a762b92
bielFernandes <gabrielfdias082@gmail.com>
2023-12-11 03:09:49 -0300

    create: (step1) instanced a base model (parent class model)

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from config.Config import config #DB CONFIG
+from src.models.base_model import * #BASE MODEL
 
 
 app = Flask(__name__)
 app.config.from_object(config) #DB CONFIG
+
+db.init_app(app) #BASE MODEL
+with app.app_context(): #CREATE BASE MODEL CHILDRENS
+    db.create_all()
 
 
 if __name__ == "__main__":
