94d8ac3fee948aa482b2912b2c98c3367960e4d9
bielFernandes <gabrielfdias082@gmail.com>
2023-12-11 03:10:20 -0300

    create: (step2) instanced a User model (children class model)

---
+++ src/models/User.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from .base_model import *
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String)
