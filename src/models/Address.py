d49c2911fc8ae0d4b1af366b7839239cbbeb1ea9
bielFernandes <gabrielfdias082@gmail.com>
2023-12-11 20:24:33 -0300

    MODEL RELATIONSHIP EXAMPLE

---
+++ src/models/Address.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base_model import Base
+
+class Address(Base):
+     __tablename__ = "address"
+
+     id: Mapped[int] = mapped_column(primary_key=True)
+     email_address: Mapped[str]
+     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+
+     user: Mapped["User"] = relationship(back_populates="addresses")
+
+     def __repr__(self) -> str:
+         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
--- src/models/User.py
+++ src/models/User.py
@@ -1,8 +1,18 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
-from .base_model import *
+from typing import List
+from typing import Optional
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base_model import Base
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String)
+class User(Base):
+     __tablename__ = "user_account"
+
+     id: Mapped[int] = mapped_column(primary_key=True)
+     name: Mapped[str] = mapped_column(String(30))
+     fullname: Mapped[Optional[str]]
+     addresses: Mapped[List["Address"]] = relationship(
+         back_populates="user", cascade="all, delete-orphan"
+     )
+
+     def __repr__(self) -> str:
+         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
