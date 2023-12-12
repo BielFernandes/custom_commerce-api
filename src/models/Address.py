00ef661d9d2223adb62b99eb610cff2f0c4dffcf
bielFernandes <gabrielfdias082@gmail.com>
2023-12-12 18:47:10 -0300

    fix: models errors

--- src/models/Address.py
+++ src/models/Address.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .base_model import Base
+from .base_model import *
 
 class Address(Base):
      __tablename__ = "address"
 
      id: Mapped[int] = mapped_column(primary_key=True)
      email_address: Mapped[str]
-     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
 
      user: Mapped["User"] = relationship(back_populates="addresses")
 
--- src/models/User.py
+++ src/models/User.py
@@ -2,10 +2,10 @@ from typing import List
 from typing import Optional
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .base_model import Base
+from .base_model import *
 
 class User(Base):
-     __tablename__ = "user_account"
+     __tablename__ = "user"
 
      id: Mapped[int] = mapped_column(primary_key=True)
      name: Mapped[str] = mapped_column(String(30))
