3f362656f238271c147400fa28d34612880566c5
BielFernandes <gabrielfdias082@gmail.com>
2023-12-13 11:24:01 -0300

    fix: adjusted User model

--- src/models/User.py
+++ src/models/User.py
@@ -1,18 +1,18 @@
 from typing import List
 from typing import Optional
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import String, Boolean, Date
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base_model import *
 
 class User(Base):
      __tablename__ = "user"
-
      id: Mapped[int] = mapped_column(primary_key=True)
-     name: Mapped[str] = mapped_column(String(30))
-     fullname: Mapped[Optional[str]]
-     addresses: Mapped[List["Address"]] = relationship(
-         back_populates="user", cascade="all, delete-orphan"
-     )
+     fname: Mapped[str] = mapped_column(String(30), nullable=False)
+     lname: Mapped[str] = mapped_column(String(30), nullable=False)
+     email: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+     password: Mapped[str] = mapped_column(String(30), nullable=False)
+     admin: Mapped[bool] = mapped_column(Boolean, default=False)
+     created_at: Mapped[str] = mapped_column(Date)
 
-     def __repr__(self) -> str:
-         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    #  def __repr__(self) -> str:
+    #      return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
