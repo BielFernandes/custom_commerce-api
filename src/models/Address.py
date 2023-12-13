b593f8f45ff2cbf2c4bf39d70c6d5bf91d8327d0
BielFernandes <gabrielfdias082@gmail.com>
2023-12-13 11:25:14 -0300

    fix: comment relationship with Address and User

--- src/models/Address.py
+++ src/models/Address.py
@@ -7,9 +7,9 @@ class Address(Base):
 
      id: Mapped[int] = mapped_column(primary_key=True)
      email_address: Mapped[str]
-     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    #  user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
 
-     user: Mapped["User"] = relationship(back_populates="addresses")
+    #  user: Mapped["User"] = relationship(back_populates="addresses")
 
-     def __repr__(self) -> str:
-         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    #  def __repr__(self) -> str:
+    #      return f"Address(id={self.id!r}, email_address={self.email_address!r})"
