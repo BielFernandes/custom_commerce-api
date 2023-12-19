240d35fae10ff54c0cea7d3af6b71257380acc54
BielFernandes <gabrielfdias082@gmail.com>
2023-12-19 15:52:20 -0300

    fix: removed prints

--- src/middlewares/auth.py
+++ src/middlewares/auth.py
@@ -18,9 +18,9 @@ def token_required(f):
           }, 401
         try:
           data=jwt.decode(token, "secret", algorithms=["HS256"])
-          print('datazinhaaaaa',data)
+          
           current_user=db.session.query(User).filter_by(email=data['email']).first()
-          print('current_user',current_user)
+          
           if current_user is None:
             return {
               "message": "Invalid Authentication token!",
@@ -28,7 +28,6 @@ def token_required(f):
               "error": "Unauthorized"
             }, 401
         except Exception as e:
-          print('erroooooooooo',e)
           return {
                 "message": "Something went wrong",
                 "data": None,
@@ -38,8 +37,6 @@ def token_required(f):
         # if find_user:
         user_schema = UserSchema()
         current_user = user_schema.dump(current_user)
-
-        print('aaaaaaaaaaaaaaaaaa', current_user)
 
         return f(current_user, *args, **kwargs)
         
